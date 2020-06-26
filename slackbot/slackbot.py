f878acce7730626f896c887e58c901cb2244392e
吉住 真菜 <yoshizumi@yoshizumi1.local>
2020-06-26 17:03:27 +0900

    表示

--- slackbot/slackbot.py
+++ slackbot/slackbot.py
@@ -9,15 +9,33 @@ BACKLOG_URL = "https://aws-plus.backlog.jp/api/v2/issues"
 BACKLOG_PARAMS = {
     'apiKey': 'un9Zxec8M0HXUARoTBaKoKXQ9mX760fUKZubf5A9dJehUfX2XSAkp3okzJTUi31w',
     'projectId[]': 1073909995,
-    #'statusId[]': {1},  # 1:未対応,2:処理中,3:処理済み,4:完了
+    # 'statusId[]': {1},  # 1:未対応,2:処理中,3:処理済み,4:完了
     'sort': 'updated',  # 更新日でソート
     'count': 10,       # 100件取得
 }
 
+will_do=[]
+doing=[]
+do=[]
+done=[]
+
 # データをリクエスト
 backlog_issues = requests.get(BACKLOG_URL, params=BACKLOG_PARAMS).json()
 
-backlog_list =  json.dumps(backlog_issues, ensure_ascii=False, indent=2)
+# backlog_list =  json.dumps(backlog_issues, ensure_ascii=False, indent=2)
+# # print(type(eval(backlog_list[0])))
 
-for task_object in backlog_list:
-    print(task_object[0]["summary"])
+for task_object in backlog_issues:
+    if task_object["status"]["id"] == 1:
+        will_do.append(task_object["summary"])
+    elif task_object["status"]["id"] == 2:
+        doing.append(task_object["summary"])
+        print(type(task_object["summary"].encode('utf-8')))
+    elif task_object["status"]["id"] == 3:
+        do.append(task_object["summary"])
+    elif task_object["status"]["id"] == 4:
+        done.append(task_object["summary"])
+    
+print(will_do)
+# print(doing)
+print(doing[0].encode('utf-8'))
