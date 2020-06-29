521c9308c5ad1da5d2864feebf919bce4f9be670
吉住 真菜 <yoshizumi@yoshizumi1.local>
2020-06-29 12:03:35 +0900

    lambdaでの実行

--- slackbot/slackbot.py
+++ slackbot/slackbot.py
@@ -1,11 +1,16 @@
 # coding:utf-8
+import os
+import sys
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'requests'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'slackweb'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'vendor'))
 import requests
 import json
 import slackweb
 
 # 取得したいBacklogのURL
 BACKLOG_URL = "https://aws-plus.backlog.jp/api/v2/issues"
-slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
 
 # 課題の内容
 BACKLOG_PARAMS = {
@@ -16,38 +21,40 @@ BACKLOG_PARAMS = {
     'count': 20,       # 100件取得
 }
 
-will_do=[]
-doing=[]
-do=[]
-done=[]
-
-# データをリクエスト
-backlog_issues = requests.get(BACKLOG_URL, params=BACKLOG_PARAMS).json()
-backlog_list =  json.dumps(backlog_issues, ensure_ascii=False, indent=2)
+def lambda_handler(event,context) :
+        
 
 
-for task_object in backlog_issues:
-    if task_object["status"]["id"] == 1:
-        summary = task_object["summary"].encode('utf-8')
-        will_do.append(summary)
-    elif task_object["status"]["id"] == 2:
-        summary = task_object["summary"].encode('utf-8')
-        print(type(summary))
-        doing.append(summary)
-    elif task_object["status"]["id"] == 3:
-        summary = task_object["summary"].encode('utf-8')
-        do.append(summary)
-    elif task_object["status"]["id"] == 4:
-        summary = task_object["summary"].encode('utf-8')
-        done.append(summary)
-    
+    will_do=list()
+    doing=list()
+    do=list()
+    done=list()
 
-id_1 = '\n'.join(will_do)
-id_2 = '\n'.join(doing)
-id_3 = '\n'.join(do)
+    # データをリクエスト
+    backlog_issues = requests.get(BACKLOG_URL, params=BACKLOG_PARAMS).json()
 
-result = "未対応\n"+ id_1 + "\n\n処理中\n" + id_2 + "\n\n処理済\n" + id_3
 
-slack.notify(text= result, channel = "#agile-training-2020")
+    for task_object in backlog_issues:
+        if task_object["status"]["id"] == 1:
+            summary = task_object["summary"].encode('utf-8')
+            will_do.append(summary.decode('utf-8'))
+        elif task_object["status"]["id"] == 2:
+            summary = task_object["summary"].encode('utf-8')
+            doing.append(summary.decode('utf-8'))
+        elif task_object["status"]["id"] == 3:
+            summary = task_object["summary"].encode('utf-8')
+            do.append(summary.decode('utf-8'))
+        elif task_object["status"]["id"] == 4:
+            summary = task_object["summary"].encode('utf-8')
+            done.append(summary.decode('utf-8'))
+
+
+    id_1 = '\n'.join(will_do)
+    id_2 = '\n'.join(doing)
+    id_3 = '\n'.join(do)
+
+    result = "未対応\n"+ id_1 + "\n\n処理中\n" + id_2 + "\n\n処理済\n" + id_3
+
+    slack.notify(text= result, channel = "#agile-training-2020")
 
 
